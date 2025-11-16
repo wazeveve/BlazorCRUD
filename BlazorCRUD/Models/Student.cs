4d6afc2cb60bec2269077f3c97b437834b3b8486
wazeveve <gabrielveanancio1123@gmail.com>
2025-11-16 14:28:29 -0300

    Editing Create Page to be Interactive and show erros.

--- BlazorCRUD/Models/Student.cs
+++ BlazorCRUD/Models/Student.cs
@@ -1,9 +1,13 @@
-﻿namespace BlazorCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCRUD.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Range(18,35)]
         public int Age { get; set; }
         public DateTime? BirthDay { get; set; }
 
